e7975a29fec641eabdf1953ecc3edae211ecbd00
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-09-01 22:42:22 +0530

    html injection done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components/macro";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Outer>
+      <h1>{process.env.REACT_APP_BROKER_NAME}</h1>
+      <p>Checkout the index.html file to see how html injection works.</p>
+    </Outer>
   );
 }
 
 export default App;
+
+const Outer = styled.div`
+  padding: 1rem;
+  margin: 1rem;
+  box-sizing: border-box;
+  animation: theme 10s ease-out infinite;
+  @keyframes theme {
+    0% {
+      border: 3px solid ${process.env.REACT_APP_TERNARY_COLOR};
+    }
+    25% {
+      border: 3px solid ${process.env.REACT_APP_PRIMARY_COLOR};
+    }
+    50% {
+      border: 3px solid ${process.env.REACT_APP_SECONDARY_COLOR};
+    }
+    75% {
+      border: 3px solid ${process.env.REACT_APP_TERNARY_COLOR};
+    }
+    100% {
+      border: 3px solid ${process.env.REACT_APP_PRIMARY_COLOR};
+    }
+  }
+`;
